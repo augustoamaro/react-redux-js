aa912db0e3a93a3ba9528ac2bd4c2b67e4cd01a9
Augusto <augustoamaro@icloud.com>
2021-10-05 17:08:53 -0300

    Created Component card #3

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,20 +1,38 @@
+import './App.css'
 import React from "react";
 
+import Card from "./components/layout/Card";
 import Primeiro from './components/basicos/Primeiro';
 import ComParametro from './components/basicos/ComParametro';
 import Fragmento from './components/basicos/Fragmento';
 import Aleatorio from "./components/basicos/Aleatorio";
 
 export default () => (
-    <div id="app">
-        <h1>Fundamentos React (Arrow)</h1>
-        <Fragmento />
-        <ComParametro
-            titulo="Situação do aluno"
-            aluno="Jurandir"
-            nota={9.3}
-        />
-        <Primeiro></Primeiro>
-        <Aleatorio min={1} max={60}/>
+    <div className="App">
+        <h1>Fundamentos React</h1>
+
+        <div className="Cards">
+
+            <Card titulo="#04 - Exemplo de Card" color="#FF7A00">
+                <Aleatorio min={1} max={60} />
+            </Card>
+
+            <Card titulo="#03 - Fragmento" color="#864000">
+                <Fragmento />
+            </Card>
+
+            <Card titulo="#02 - Com parâmetro" color="#FF7A00">
+                <ComParametro
+                    titulo="Situação do aluno"
+                    aluno="Jurandir"
+                    nota={9.3}
+                />
+            </Card>
+
+            <Card titulo="#01 - Primeiro Componente" color="#864000">
+                <Primeiro></Primeiro>
+            </Card>
+
+        </div>
     </div>
 );
